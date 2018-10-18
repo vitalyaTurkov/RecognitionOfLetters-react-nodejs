ba6c12563c0c61d0dd66e05e236d82ecf0497473
vitalyaTurkov <vitaliatyrkov@yandex.ru>
2018-10-19 06:37:35 +0700

    add axios

--- client/src/service/api/sendImage.js
+++ client/src/service/api/sendImage.js
@@ -1,23 +1,10 @@
 import config from '../../etc/config'
-
+import axios from 'axios'
 //Отправка кода картинки на сервер
 
 export default function sendImage(canvas) {
-    return new Promise(resolve => {
-        const xhr = new XMLHttpRequest();
-        let img = new Image();
-        img.src = canvas.toDataURL('image/png');
 
-        xhr.open("POST", `${config.url}/image`, true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(JSON.stringify({image: img.src}));
-
-        xhr.onload = function() {
-            try {
-                resolve(this.responseText);
-            } catch (e) {
-                console.log("uncorrected data from server ", e);
-            }
-        };
-    });
+    let img = new Image();
+    img.src = canvas.toDataURL('image/png');
+    return axios.post(`${config.url}/image`, {image: img.src});
 }
